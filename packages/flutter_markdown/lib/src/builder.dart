7fc0e0295d6fca81d71c9684892a18126e40f5eb
Tanmoy Sarkar <57363826+Tanmoy741127@users.noreply.github.com>
2022-12-10 01:52:30 +0530

    If there is no widgets in _inline element, just discard that

--- packages/flutter_markdown/lib/src/builder.dart
+++ packages/flutter_markdown/lib/src/builder.dart
@@ -191,10 +191,13 @@ class MarkdownBuilder implements md.NodeVisitor {
     // If there is one inline element left behind, wrap that in a column and add it to the block.
     if (_inlines.length == 1) {
       List<Widget> widgets = _inlines.first.children;
-      Widget col = Column(
-        children: widgets,
-      );
-      _blocks.single.children.add(col);
+      if(widgets.length == 0) _inlines.clear();
+      else{
+        Widget col = Column(
+          children: widgets,
+        );
+        _blocks.single.children.add(col);
+      }
     }
 
 
